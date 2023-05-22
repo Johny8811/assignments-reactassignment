decefac40fdd88a16755625639a7ec7d8286bb5b
Jan Hujsa <janhujsa@JanPro-M1.local>
2023-05-22 15:46:29 +0200

    F2: Add loading enum

--- client/src/queries/enpoints.ts
+++ client/src/queries/enpoints.ts
@@ -1,3 +1,10 @@
 const BASE = "http://localhost:3000";
 
 export const TODO_ITEMS = `${BASE}/items`;
+
+export enum LOADING_ENUM {
+    ADD_TODO_ITEM = `POST:${TODO_ITEMS}`,
+    GET_TODO_ITEMS = `GET:${TODO_ITEMS}`,
+    EDIT_TODO_ITEM = `PATCH:${TODO_ITEMS}`,
+    DELETE_TODO_ITEMS = `DELETE:${TODO_ITEMS}`,
+}
