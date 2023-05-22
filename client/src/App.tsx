6eb23a209e2c432bbbf0d30a875a7d12cfa0ae70
Jan Hujsa <janhujsa@JanPro-M1.local>
2023-05-22 16:17:24 +0200

    F2: Fetch and show list of all todo items

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,33 @@
-import { useCallback } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 import { Container } from "./components/Container";
 import { Layout } from "./components/Layout";
 import { List } from "./components/List";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
+import { ListItem } from "./components/ListItem";
 import { ThemeProvider } from "./components/ThemeProvider";
 import { addTodoItemHook } from "./queries/addTodoItemHook";
+import { getTodoItemsHook, Todos } from "./queries/getTodoItemsHook";
 
 export const App: React.FC = () => {
+    const [todoItems, setTodoItems] = useState<Todos>();
+
     const addTodoItem = addTodoItemHook();
+    const getTodoItems = getTodoItemsHook();
 
     const handleAddItem = useCallback((v: string) => {
         void addTodoItem({
             done: false,
             title: v,
         });
+    }, []);
+
+    useEffect(() => {
+        getTodoItems().then((data) => {
+            setTodoItems(data);
+        });
+        // TODO: improvements: handle catch error
     }, []);
 
     return (
@@ -25,7 +37,9 @@ export const App: React.FC = () => {
                     <span>
                         <Header handleAddItem={handleAddItem}>To Do app</Header>
                         <List>
-                            <div>abc</div>
+                            {todoItems?.map((i) => (
+                                <ListItem key={i.id} label={i.title} handleEdit={() => {}} handleRemoval={() => {}} />
+                            ))}
                         </List>
                     </span>
                     <Footer />
